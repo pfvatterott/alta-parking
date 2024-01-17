4adc0b9f34caffbce87bd360d38a023e702d3b07
Paul Vatterott <pfvatterott@gmail.com>
2024-01-17 09:26:22 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const axios = require('axios');
 
 const data = {
-    "cart_id": 80846755,
-    "override_conflict": true,
-    "source": "web-app",
-    "vehicle": 9572570,
-    "oa_tag": "2360b00d8c6a476e850525bf076ad275",
-    "honkGUID": "yjze4ofcuodwzxbo57ecjm",
-    "app_version": 69000
-  };
+  "cart_id": 80859971,
+  "override_conflict": true,
+  "source": "web-app",
+  "vehicle": 9572570,
+  "oa_tag": "2360b00d8c6a476e850525bf076ad275",
+  "honkGUID": "op4ivnqlmi8f9tch6nzn2v",
+  "app_version": 69000
+};
 
 const config = {
   headers: {
@@ -17,21 +17,16 @@ const config = {
 };
 
 async function postRequest() {
-    try {
-      const response = await axios.post('https://platform.honkmobile.com/parking_sessions', data, config);
-      console.log(`Status: ${response.status}`);
-      console.log(`Body: ${JSON.stringify(response.data)}`);
-      return True
-    } catch (error) {
-      console.error(error.response.data.errors);
-      return False
-    }
+  try {
+    const response = await axios.post('https://platform.honkmobile.com/parking_sessions', data, config);
+    console.log(`Status: ${response.status}`);
+    console.log(`Body: ${JSON.stringify(response.data)}`);
+    exit()
+  } catch (error) {
+    console.error(`${Date.now()} - ${error.response.data.errors}`);
   }
+}
 
-
-const delay = ms => new Promise(res => setTimeout(res, ms));
-let isPurchased = False
-while (!isPurchased)
-    isPurchased = postRequest()
-    await delay(5000)
-
+setInterval(function() {
+  postRequest()
+}, 3000)
